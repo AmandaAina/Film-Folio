03d9a6ffa2289435546192fc571a9aafb60f0c99
Anthony <anthony.l.michel@gmail.com>
2024-01-02 20:43:44 -0500

    added review seed, revised code

--- seeds/index.js
+++ seeds/index.js
@@ -1,11 +1,13 @@
 const sequelize = require('../config/connection');
 const seedUsers = require('./userData');
 const seedMovies = require('./movieData');
+const seedReviews = require('./reviewData');
 
 const seedDatabase = async () => {
-  await sequelize.syn({ force: true });
+  await sequelize.sync({ force: true });
   await seedUsers();
   await seedMovies();
+  await seedReviews();
 
   process.exit(0);
 };
