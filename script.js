482dfba4cba15f6766f2a729532463d5548f8d61
Amanda Aina <amandaade92@yahoo.com>
2024-01-02 19:37:25 -0500

    html folder

--- script.js
+++ script.js
@@ -3,10 +3,13 @@ function validateLogin() {
     var password = document.getElementById('password').value;
 
     // Simple validation (replace this with more robust validation)
-    if (username === 'your_username' && password === 'your_password') {
+    if (username === 'test' && password === 'test') {
         // Redirect to the homepage 
-        window.location.href = 'homepage.html';
+        window.alert('Login successful! Redirecting to homepage...');
     } else {
         alert('Invalid credentials. Please try again.');
+    }
+    function validateLogin() {
+        alert('Button clicked!'); // Add this line to test if the script is executing
     }
 }
