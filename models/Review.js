326f883ebd14dabaf732a8fe1fac7c150be7fe78
Anthony <anthony.l.michel@gmail.com>
2024-01-02 19:35:44 -0500

    added review model

---
+++ models/Review.js
@@ -0,0 +1,48 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Review extends Model {}
+
+Review.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        reviewText: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        dateCreated: {
+            type: DataTypes.DATEONLY,
+            allowNull: false,
+            defaultValue: DataTypes.NOW
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        },
+        movie_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'movie',
+                key: 'id'
+            }
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'review',
+    }
+);
+
+module.exports = Review;
